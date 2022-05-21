c2968de76b6da12a2c97050427f359044b44d8c3
sunggyu <treelee96@naver.com>
2022-05-21 21:42:21 +0900

    first_commit
    first_commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,6 @@
-// // 마우스 휠
-// window.addEventListener(
-//   "wheel",
-//   function (e) {
-//     e.preventDefault();
-//   },
-//   { passive: false }
-// );
-
-// 페이지 선언
-var $html = $("html");
-var page = 1;
-var lastPage = $(".contact").length;
-
-$html.animate({ scrollTop: 0 }, 10);
-
-//휠 굴렸을 때 이벤트
-$(window).on("wheel", function (e) {
-  if ($html.is(":animated")) return;
-
-  if (e.originalEvent.deltaY > 0) {
-    if (page == lastPage) return;
-
-    page++;
-  } else if (e.originalEvent.deltaY < 0) {
-    if (page == 1) return;
-
-    page--;
-  }
-  var posTop = (page - 1) * $(window).height();
-
-  $html.animate({ scrollTop: posTop });
-});
+$("body").animate(
+  {
+    scrollTop: 300,
+  },
+  500
+);
